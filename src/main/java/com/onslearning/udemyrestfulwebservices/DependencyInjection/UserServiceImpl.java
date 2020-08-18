7e0c95634b500b3f92b0a4617ed0e751e996ac3c
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-08-18 14:43:06 -0600

    Dependency injection by Constructor and Setter method
    Create a method to get object of "getUserID" class using constructor based DI and Setter Based DI.

--- src/main/java/com/onslearning/udemyrestfulwebservices/DependencyInjection/UserServiceImpl.java
+++ src/main/java/com/onslearning/udemyrestfulwebservices/DependencyInjection/UserServiceImpl.java
@@ -1,18 +1,42 @@
 package com.onslearning.udemyrestfulwebservices.DependencyInjection;
 
 import com.onslearning.udemyrestfulwebservices.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    getUserID userIDObject;
+
+    /**
+     * Constructor Based Dependency Injection.
+     */
+    @Autowired
+    public UserServiceImpl(getUserID userIDObject){
+        this.userIDObject = userIDObject;
+    }
+
+    public UserServiceImpl() {
+    }
+
+    /**
+     * Setter Based Dependency Injection.
+     * @param userIDObject
+     */
+    @Autowired
+    public void setUserIDObject(getUserID userIDObject) {
+        this.userIDObject = userIDObject;
+    }
+
     @Override
     public User createUserDI(User userObjectFromURL) {
 
-        User userDI = new User(userObjectFromURL.getUserID(), userObjectFromURL.getUserName(),
+        User userDI = new User(userIDObject.createUserID(), userObjectFromURL.getUserName(),
                                 userObjectFromURL.getAge(), userObjectFromURL.getSalary());
         return userDI;
     }
+
 
 
 }
--- src/main/java/com/onslearning/udemyrestfulwebservices/DependencyInjection/getUserID.java
+++ src/main/java/com/onslearning/udemyrestfulwebservices/DependencyInjection/getUserID.java
@@ -1,5 +1,12 @@
 package com.onslearning.udemyrestfulwebservices.DependencyInjection;
 
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class getUserID {
-    
+  public String createUserID(){
+      return UUID.randomUUID().toString();
+  }
 }
